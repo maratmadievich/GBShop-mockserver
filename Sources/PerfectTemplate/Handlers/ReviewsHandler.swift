1e4c6c3fe82d58eccb68f378d64a1638338958cd
maratmadievich <46650026+maratmadievich@users.noreply.github.com>
2019-05-27 21:07:34 +0400

    ReviewsHandler ответ находится внутри handler

--- Sources/PerfectTemplate/Handlers/ReviewsHandler.swift
+++ Sources/PerfectTemplate/Handlers/ReviewsHandler.swift
@@ -9,6 +9,11 @@ import PerfectHTTP
 
 class ReviewsHandler: AbstractHandler {
     
+    private let baseLimit = 20
+    private let baseMaxRowsCount = 60
+    private let baseUserName = "defaultUser"
+    private let baseReview = "default review about product"
+    
     func process(request: HTTPRequest, response: HTTPResponse) {
         
         guard let idProduct = Int(request.params(named: "id_product").first ?? "") else {
@@ -22,10 +27,23 @@ class ReviewsHandler: AbstractHandler {
         }
         
         response.setHeader(.contentType, value: "text/html")
-        let responseString = GetReviewsDefaultResponse.instance.generateResponse(idProduct: idProduct, pageNumber: pageNumber)
+        let responseString = generateResponse(idProduct: idProduct, pageNumber: pageNumber)
         
         print(responseString)
         response.appendBody(string: responseString)
         response.completed()
+    }
+    
+    private func generateResponse(idProduct: Int, pageNumber: Int) -> String {
+        
+        return "{\"page_number\": \(pageNumber), \"max_rows_count\": \(baseMaxRowsCount), \"reviews\": [\(generateReviews(idProduct: idProduct, pageNumber: pageNumber))]}"
+    }
+    
+    private func generateReviews(idProduct: Int, pageNumber: Int) -> String {
+        if pageNumber < 3 {
+            return Array(1...baseLimit)
+                .reduce("") {(totalString, number) in totalString + "{\"id_product\": \(idProduct), \"id_user\": \(number), \"user_name\": \"\(baseUserName)\(baseLimit * pageNumber + number)\", \"text\": \"\(baseReview)\(baseLimit * pageNumber + number)\"}\(number < baseLimit ? ", " : "")"}
+        }
+        return ""
     }
 }
