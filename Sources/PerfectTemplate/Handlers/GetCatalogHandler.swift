9580e8f96259ee9f45bcb8ff6b8617069750cd3a
maratmadievich <46650026+maratmadievich@users.noreply.github.com>
2019-05-27 21:08:55 +0400

    GetCatalogHandler ответ находится внутри handler

--- Sources/PerfectTemplate/Handlers/GetCatalogHandler.swift
+++ Sources/PerfectTemplate/Handlers/GetCatalogHandler.swift
@@ -9,6 +9,10 @@ import PerfectHTTP
 
 class GetCatalogHandler: AbstractHandler {
     
+    private let baseLimit = 20
+    private let baseMaxRowsCount = 60
+    private let baseProductName = "geekbrainsProduct"
+    
     func process(request: HTTPRequest, response: HTTPResponse) {
         
         guard let pageNumber = Int(request.params(named: "page_number").first ?? "") else {
@@ -26,14 +30,25 @@ class GetCatalogHandler: AbstractHandler {
         }
         
         response.setHeader(.contentType, value: "text/html")
-        
-        let responseString = GetCatalogDefaultResponse.instance.generateResponse(pageNumber: pageNumber)
-        
+        let responseString = generateResponse(pageNumber: pageNumber)
         print(responseString)
         
         response.appendBody(string: responseString)
-        
         response.completed()
+    }
+    
+    private func generateResponse(pageNumber: Int) -> String {
+        return "{\"page_number\": \(pageNumber), \"max_rows_count\": \(baseMaxRowsCount), \"products\": [\(generateProducts(pageNumber: pageNumber))]}"
+    }
+    
+    private func generateProducts(pageNumber: Int) -> String {
+        
+        if pageNumber < 3 {
+            
+            return Array(1...baseLimit)
+                .reduce("") {(totalString, number) in totalString + "{\"id_product\": \(baseLimit * pageNumber + number), \"product_name\": \"\(baseProductName)\(baseLimit * pageNumber + number)\", \"price\": \(number * 100)}\(number < baseLimit ? ", " : "")"}
+        }
+        return ""
     }
     
 }
